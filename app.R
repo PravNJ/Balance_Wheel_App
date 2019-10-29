f88fe7d77caf9edc883bff988ceca7b646919676
PravNJ <prav.nj@gmail.com>
2019-10-29 15:43:10 +1100

    Project set up for Balance Wheel visualiser

---
+++ app.R
@@ -0,0 +1,85 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Balance Wheel Prototype"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Love:",
+                        min = 1,
+                        max = 10,
+                        value = 1),
+            sliderInput("bins",
+                        "MentalHealth:",
+                        min = 1,
+                        max = 10,
+                        value = 1),
+            sliderInput("bins",
+                        "Community:",
+                        min = 1,
+                        max = 10,
+                        value = 1),
+            sliderInput("bins",
+                        "Family:",
+                        min = 1,
+                        max = 10,
+                        value = 1),
+            sliderInput("bins",
+                        "Career:",
+                        min = 1,
+                        max = 10,
+                        value = 1),
+            sliderInput("bins",
+                        "Money:",
+                        min = 1,
+                        max = 10,
+                        value = 1),
+            sliderInput("bins",
+                        "Physical Health:",
+                        min = 1,
+                        max = 10,
+                        value = 1),
+            sliderInput("bins",
+                        "Purpose:",
+                        min = 1,
+                        max = 10,
+                        value = 1)
+            
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
